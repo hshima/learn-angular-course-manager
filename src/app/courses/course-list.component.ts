d1881685d2a2a5c59fc9bcaf9e557f625490c913
hshimada <shimada.henrique@gmail.com>
2020-06-24 16:22:28 -0300

    Adition of images to the project

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,14 +1,42 @@
-import { Component } from '@angular/core'; // Imports Decorator that describes what will the @Component be 
+import { Component, OnInit } from '@angular/core'; // Imports Decorator that describes what will the @Component be 
+import { Course } from './course';
 
 @Component({
     selector: 'app-course-list', // tag name to be referenced to access this class
     //template: '<h2>Course List</h2>' // Static component
     templateUrl: './course-list.component.html' // Aplies reponsibility segregation, so static content won't be stored in this code
-    
+
 })
 
-export class CourseListComponent { // Allows class to other files to reference this class
+export class CourseListComponent implements OnInit {
+    // 'export' Allows class to other files to reference this class
+    // Interface OnInit inicia o componente assim que criado
 
+    courses: Course[] = [];
+    ngOnInit(): void { //permite adicionar tarefas para inicialização do componente da interface
+        this.courses = [
+            {
+                id: 1,
+                name: 'Angular: Forms',
+                imageURL: '/assets/images/forms.png',
+                price: 99.99,
+                code: 'QWERTY',
+                duration: 120,
+                rating: 5.4,
+                releasedDate: 'December, 2, 2019'
+            },
+            {
+                id: 2,
+                name: 'Angular: HTTP',
+                imageURL: '/assets/images/http.png',
+                price: 57.99,
+                code: 'WERTYU',
+                duration: 80,
+                rating: 5.5,
+                releasedDate: 'December, 4, 2019'
+            }
+        ]
 
+    }
 
 }
