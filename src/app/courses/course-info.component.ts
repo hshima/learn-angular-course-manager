564cb76798372e27d9fb04cae954f8a9d815c042
hshimada <shimada.henrique@gmail.com>
2020-06-26 14:33:32 -0300

    adding angular form sprites

--- src/app/courses/course-info.component.ts
+++ src/app/courses/course-info.component.ts
@@ -1,22 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Course } from './course';
+import { CourseService } from './course.service';
 
 @Component({
     templateUrl: './course-info.component.html'
 })
 export class CourseInfoComponent implements OnInit {
 
-    courseId: number;
+    course: Course;
 
-    constructor(private activeRoute: ActivatedRoute) { }
+    constructor(
+        private activeRoute: ActivatedRoute,
+        private courseService: CourseService) { }
 
     ngOnInit(): void {
-        this.courseId = + //converts a string to number
-        this.activeRoute // From the actual route,
-        .snapshot // take a snapshot from the object
-        .paramMap // from the object, take parameters
-        .get('id') // which matches the id
-
+        this.course = this.courseService.retrieveById(
+            + //converts a string to number
+            this.activeRoute // From the actual route,
+                .snapshot // take a snapshot from the object
+                .paramMap // from the object, take parameters
+                .get('id') // which matches the id
+        )
     }
 
 }
