f1604a861eb235fbf8229ddc8dcc5e79f6a7e727
hshimada <shimada.henrique@gmail.com>
2020-06-27 21:21:26 -0300

    Adding core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { Error404Component } from './error-404/error-404.component';
 import { CourseModule } from './courses/course.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
-    NavBarComponent,
     Error404Component,
     AppComponent // references AppComponent Class in app.component.ts
   ],
@@ -18,6 +17,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     HttpClientModule,
     CourseModule,
+    CoreModule,
     RouterModule.forRoot([ // Declares an array of routes that will be initalized by the begining of the aplication
       {
         path: '', redirectTo: 'courses', pathMatch: 'full' // When in the root of the aplication (''), redirect to route courses. The path match must be full (specific case for routes)
